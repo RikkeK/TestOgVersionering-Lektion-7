c39729972ad07e511138add16775e75409334347
Rikke <Rikke@10.16.105.228>
2015-10-19 16:56:59 +0200

    Oprettet test: testAtManVedÆndringAfBilletprisIkkeÆndrePrisForTidlige...

--- src/automat/UnitsTestAfBilletautomat.java
+++ src/automat/UnitsTestAfBilletautomat.java
@@ -47,18 +47,27 @@ public class UnitsTestAfBilletautomat {
 		assertEquals(10, automat.getBilletpris());
 	}
 		
+	
 	@Test
 	//Tjek af at man ikke ændre billetpris uden at være logget ind som montør
-	public void testAtManIkkeKanÆndreBilletprisUdenAtVæreLoggetInd() {
+	public void testAtManIkkeKanÆndreBilletPrisUdenAtVæreLoggetInd() {
 		Billetautomat automat = new Billetautomat();
 		automat.setBilletpris(20);
 		assertEquals(10, automat.getBilletpris());	
+	}
 	
-
-		
-	
-		
-		
+	@Test
+	//Tjek at ved ændring af billetpris, ikke ændre pris for de tidligere købte billeter
+	public void testAtManVedÆndringAfBilletprisIkkeÆndrePrisForTidligereKøbteBilleter() {
+		Billetautomat automat = new Billetautomat();
+		automat.indsætPenge(20);
+		automat.udskrivBillet();
+		automat.montørLogin("1234"); 
+		int gammelindkomst = automat.getTotal();
+		automat.setBilletpris(100);
+		int nyindkomst = automat.getTotal();
+		assertEquals(gammelindkomst, nyindkomst);		
+		}
 	}
 
-}
+

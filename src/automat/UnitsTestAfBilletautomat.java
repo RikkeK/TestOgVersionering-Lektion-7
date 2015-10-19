8683cab4d76f3395e6f3138cc2caef57429a7359
Rikke <Rikke@10.16.105.228>
2015-10-19 16:08:25 +0200

    Oprettet en UnitsTest

---
+++ src/automat/UnitsTestAfBilletautomat.java
@@ -0,0 +1,21 @@
+package automat;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UnitsTestAfBilletautomat {
+
+	@Test
+	public void testGetBilletpris() {
+		Billetautomat b = new Billetautomat();
+		assertEquals(10, b.getBilletpris());
+	}
+	
+	@Test
+	public void testGetBilletpris() {
+		Billetautomat b = new Billetautomat();
+		assertEquals(10, b.getBilletpris());
+	}
+
+}

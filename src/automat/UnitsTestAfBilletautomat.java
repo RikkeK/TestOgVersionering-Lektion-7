fdbc7c1c3006a04b11c3901fc22ef9763dd3051e
Rikke <Rikke@10.16.105.228>
2015-10-19 16:32:57 +0200

    Oprettet test: testAtManIkkeKanSætteBilletprisTilNegativeVærdier

--- src/automat/UnitsTestAfBilletautomat.java
+++ src/automat/UnitsTestAfBilletautomat.java
@@ -18,6 +18,34 @@ public class UnitsTestAfBilletautomat {
 		automat.udskrivBillet();
 		assertEquals(0, automat.returpenge());
 		
+	}
+		
+//	@Test
+	//Tjek at man ikke kan købe billet hvis en billet hvis balancen er lig 0 eller under billetprisen
+//	public void testKanIkkeKøbeBilletHvisBalanceErUnderBilletpris() {
+//		Billetautomat automat = new Billetautomat();
+//		automat.udskrivBillet();
+//		assertEquals(0, automat.returpenge());	
+		
+		/*
+		 * if(automat.getBalance() <= automat.getBilletpris()){
+			System.out.println("Balancen er under eller lig 0: " + automat.getBalance());
+			int oldbalance = automat.getBalance();
+			automat.udskrivBillet(); // køb billet
+			if(automat.getBalance() == oldbalance - automat.getBilletpris()){
+				System.out.println("FEJL, du har netop købt en billet og har en negativ balance, "
+						+ "din balance er: " + automat.getBalance());
+				antalFejl = antalFejl + 1;
+			}
+		 */
+	
+	@Test
+	//Tjek af at man ikke kan sætte en billet til negative værdier
+	public void testAtManIkkeKanSætteBilletprisTilNegativeVærdier() {
+		Billetautomat automat = new Billetautomat();
+		automat.setBilletpris(-20);
+		assertEquals(10, automat.getBilletpris());
+	
 
 		
 	
